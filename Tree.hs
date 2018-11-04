e11e0f1780ec242b13cbe3746a42bed84e68e9ed
Broshan <gosha-saf@yandex.ru>
2018-11-04 22:58:44 +0300

    eqToSys

--- Tree.hs
+++ Tree.hs
@@ -1,8 +1,5 @@
-
-data Const = C Int
-    deriving (Show,Eq,Read)
-data Formula =Variable
-			         | Const
+data Formula =V Int
+               | C Int
 			         | Not Formula
 			         | Conj {
                         l :: Formula ,
@@ -13,12 +10,21 @@ data Formula =Variable
                         r ::  Formula
                       }
     deriving (Show,Eq,Read)
-data Variable = V Int
-	deriving (Show,Eq,Read)
 data Equation = Eq {
-                      l :: Formula ,
-                      r :: Formula
+                      le :: Formula ,
+                      re :: Formula
                    }
-	deriving (Show,Eq,Reads)
-newtype System = [Equation]
 	deriving (Show,Eq,Read)
+eqToSys :: Equation ->[Equation]
+eqToSys a@(Eq (V x) y) = [a]
+eqToSys a@(Eq (C x) y) = [a]
+eqToSys a@(Eq x (V y)) = [a]
+eqToSys a@(Eq x (C y)) = [a]
+eqToSys (Eq (Not x) (Not y)) = [Eq x y]
+eqToSys (Eq (Disj x1 x2) (Disj y1 y2)) = eqToSys (Eq x1 y1) ++ eqToSys (Eq x2 y2)
+eqToSys (Eq (Conj x1 x2) (Conj y1 y2)) = eqToSys (Eq x1 y1) ++ eqToSys (Eq x2 y2)
+eqToSys _ =[]
+
+
+
+
