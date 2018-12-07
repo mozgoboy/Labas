bf648e70558fa270bcc1cfbd055545e825fd324d
Broshan <gosha-saf@yandex.ru>
2018-12-07 20:26:32 +0300

    Код для алгоритма 1 и определение Vars

--- Tree.hs
+++ Tree.hs
@@ -1,3 +1,4 @@
+import Data.Foldable
 data Formula =V Int
                | C Int
                | Not Formula
@@ -22,24 +23,26 @@ data Equation = Eq {
 
 eqToSys :: Equation ->[Equation]
 eqToSys a@(Eq (V x) y) = [a]
-eqToSys a@(Eq (C x) y) = [a]
 eqToSys a@(Eq x (V y)) = [(Eq (V y) x)]
+eqToSys a@(Eq (C x) y) = [a]
 eqToSys a@(Eq x (C y)) = [(Eq (C y) x)]
 eqToSys (Eq (Not x) (Not y)) = [Eq x y]
-eqToSys (Eq (Disj x1 x2) (Disj y1 y2)) = eqToSys (Eq x1 y1) ++ eqToSys (Eq x2 y2)
-eqToSys (Eq (Conj x1 x2) (Conj y1 y2)) = eqToSys (Eq x1 y1) ++ eqToSys (Eq x2 y2)
+eqToSys (Eq (Disj x1 x2) (Disj y1 y2)) | (eqToSys (Eq x1 y1)==[]) || (eqToSys (Eq x2 y2)==[]) = []
+                                       | otherwise = eqToSys (Eq x1 y1) ++ eqToSys (Eq x2 y2)
+eqToSys (Eq (Conj x1 x2) (Conj y1 y2)) | (eqToSys (Eq x1 y1)==[]) || (eqToSys (Eq x2 y2)==[]) = []
+                                       | otherwise = eqToSys (Eq x1 y1) ++ eqToSys (Eq x2 y2)
 eqToSys _ =[]
 
 
-
-putInFormula :: Equation -> Formula -> Formula
-putInFormula _ (C z) = (C z)
-putInFormula (Eq (V x) y) (V z) | z==x = y
-                           | otherwise = V z
-putInFormula x (Not f)= Not (putInFormula x f)
-putInFormula x (Disj f g)= Disj (putInFormula x f) (putInFormula x g)
-putInFormula x (Conj f g)= Conj (putInFormula x f) (putInFormula x g)
-
-
-putInEq :: Equation -> Equation -> Equation
-putInEq x (Eq y z) = Eq (putInFormula x y) (putInFormula x z)                         
+vars :: Formula -> [Formula]
+vars f = delpov (vars0 f)
+   where
+   vars0 (V x) =[(V x)]
+   vars0 (C x) =[]
+   vars0 (Not f) = vars f
+   vars0 (Disj f g)= (vars f) ++ (vars g)
+   vars0 (Conj f g)= (vars f) ++ (vars g)
+   delpov [] =[]
+   delpov [f] = [f]
+   delpov (f:as) | elem f as = (delpov as)
+                 | otherwise = [f] ++ (delpov as)
